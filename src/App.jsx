d86b8aa10f23b37e116676e809f588e00083e001
KeithPetr <keith.petryshyn@gmail.com>
2023-09-06 00:31:20 -0400

    Removed unnecessary useEffect and useAuthState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Welcome from "../components/Welcome";
 import NavBar from "../components/NavBar";
 import ChatBox from "../components/ChatBox";
@@ -6,23 +5,7 @@ import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // Add an observer to listen to changes in the auth state
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        // User is signed in.
-        setUser(authUser);
-      } else {
-        // No user is signed in.
-        setUser(null);
-      }
-    });
-
-    // Don't forget to unsubscribe when the component unmounts
-    return () => unsubscribe();
-  }, []);
+  const [user] = useAuthState(auth);
 
   return (
     <div className="app-container">
